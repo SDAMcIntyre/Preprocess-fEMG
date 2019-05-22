c2056c23e0baa696eef4f094f4d9bcb18563c47e
Sarah McIntyre <sarah.mcintyre@liu.se>
2019-05-22 19:06:32 +0200

    loops through all raw data files and finds unexpected stimulus code triggers

--- read-femg.R
+++ read-femg.R
@@ -1,78 +1,67 @@
 library(tidyverse)
+library(readxl)
 
-rawDataFolder <- './rawdata/'
+possibleStimCodes <- read_excel('trigger stimulus coding.xlsx')$code %>% c(0)
+
+rawDataFolder <- './raw data/'
 rawDataFiles <- dir(rawDataFolder)
 femg.ChannelNames <- c('A-ZYG Processed',
                        'A-COR Processed')
 stim.ChannelName <- 'Stimulus'
 baseline.time.sec <- 0.5
 
-raw.File <- paste0(rawDataFolder,rawDataFiles[1])
+for (n in 1:length(rawDataFiles)) {
+  raw.File <- paste0(rawDataFolder,rawDataFiles[n])
+  print( paste( n, 'of', length(rawDataFiles), ':', rawDataFiles[n]))
+  
+  # extract numbers from 2nd row to get sample duration in ms and convert to sampling rate
+  sampRate.Hz <- read_lines(raw.File, skip = 1, n_max = 1) %>% 
+    str_extract('[0-9]+') %>% 
+    as.numeric() %>% 
+    ( function(x) (1 / (x / 1000)) )
+   
+  # extract numbers from 3rd row to get number of channels in file
+  raw.nChannels <- read_lines(raw.File, skip = 2, n_max = 1) %>% 
+    str_extract('[0-9]+') %>% 
+    as.numeric()
+  
+  # read channel names 
+  # just get the odd ones for the names (even rows are measurement units)
+  raw.ChannelNames <- read_lines(raw.File, skip = 3, n_max = 2*raw.nChannels
+                               )[seq(1,raw.nChannels*2, 2)]
+  
+  # get the column numbers for the femg and stimulus channels by matching the names
+  keepColumns <- c(str_which(raw.ChannelNames, stim.ChannelName),
+                   str_which(raw.ChannelNames, paste(femg.ChannelNames, collapse = '|')) )
+  
+  # read in the data, keep only the stim and femg channels
+  femg.data <- read_csv(raw.File, skip = raw.nChannels*2+5, col_names = FALSE
+                        )[,keepColumns]
+  # rename the columns
+  names(femg.data) <- c(stim.ChannelName, femg.ChannelNames)
+  
+  # add time and transition variables
+  femg.data <- femg.data %>% 
+    mutate(time.sec = seq(0,n()-1)/sampRate.Hz,
+           transition = c(1,diff(Stimulus)) != 0 | c(diff(Stimulus),1) != 0,
+           badStim = Stimulus %in% possibleStimCodes == FALSE
+           )
 
-# extract numbers from 2nd row to get sample duration in ms and convert to sampling rate
-sampRate.Hz <- read_lines(raw.File, skip = 1, n_max = 1) %>% 
-  str_extract('[0-9]+') %>% 
-  as.numeric() %>% 
-  ( function(x) (1 / (x / 1000)) )
- 
-# extract numbers from 3rd row to get number of channels in file
-raw.nChannels <- read_lines(raw.File, skip = 2, n_max = 1) %>% 
-  str_extract('[0-9]+') %>% 
-  as.numeric()
-
-# read channel names 
-# just get the odd ones for the names (even rows are measurement units)
-raw.ChannelNames <- read_lines(raw.File, skip = 3, n_max = 2*raw.nChannels
-                             )[seq(1,raw.nChannels*2, 2)]
-
-# get the column numbers for the femg and stimulus channels by matching the names
-keepColumns <- c(str_which(raw.ChannelNames, stim.ChannelName),
-                 str_which(raw.ChannelNames, paste(femg.ChannelNames, collapse = '|')) )
-
-# read in the data, keep only the stim and femg channels
-femg.data <- read_csv(raw.File, skip = raw.nChannels*2+3
-                      )[,keepColumns]
-# rename the columns
-names(femg.data) <- c(stim.ChannelName, femg.ChannelNames)
-
-# add a time variable
-femg.data <- femg.data %>% 
-  mutate(time.sec = seq(0,n()-1)/sampRate.Hz)
-
-femg.data %>% 
-  group_by(Stimulus) %>% 
-  tally() %>% 
-  mutate(sec = n/sampRate.Hz) %>% View()
-
-windows()
-femg.data[-1,] %>% 
-  ggplot(aes(time.sec, Stimulus)) +
-  geom_path()
-
-# # old code
-#   x$transition <- c(0,diff(as.logical(x$stim.trigger)))
-#   x$trial <- 0
-#   x$trial[x$transition > 0] <- seq_along(x$stim.trigger[x$transition > 0])
-#   cued <- stimNames[x$stim.trigger[x$transition > 0]]
-#   
-#  # otherwise use the auto trigger
-#       t.start <- x$time.sec[x$trial == trialNum & x$transition == 1]
-#       t.next <- x$time.sec[x$trial == trialNum +1 & x$transition == 1]
-#       if (length(t.next) == 0) t.next <- max(x$time.sec)
-#       t.end <- x$time.sec[x$time.sec > t.start & x$time.sec < t.next & x$transition == -1]
-# 
-#     
-#     x$trial[x$time.sec >= t.start -1 & x$time.sec < t.end] <- trialNum
-#     x$phase[x$time.sec >= t.start & x$time.sec < t.end] <- 'stimulus'
-#     x$phase[x$time.sec >= t.start -1 & x$time.sec < t.start] <- 'baseline'
-#     x$cued[x$trial == trialNum] <- cued[trialNum]
-#   
-#   x <- x[is.na(x$phase)==FALSE,]
-#   x <- x %>%
-#     group_by(trial) %>%
-#     mutate(time.sec = time.sec - min(time.sec[phase!='baseline']))
-#   femgData.raw <- rbind(femgData.raw,x)
-# }
-# femgData.raw <- femgData.raw[,c(1:4,6:7,9:11)]
-# femgData.raw <- merge(femgData.raw, commForfemg, all.x = TRUE)
-# 
+  raw.badStimCodes <- unique(filter(femg.data,badStim)$Stimulus)
+  
+  if (length(raw.badStimCodes) > 0) {
+    # plot the stimulus sequence
+    windows(20,3)
+    femg.data %>% 
+      filter(transition) %>% 
+      ggplot(aes(time.sec, Stimulus)) +
+      geom_path() +
+      geom_point(aes(colour = badStim)) +
+      scale_colour_manual(values = c('grey','red')) +
+      labs(title = paste(raw.badStimCodes, collapse = ', '))
+    ggsave(paste0('./bad stim codes/',rawDataFiles[n],'.png'))
+    dev.off()
+    tibble(code = raw.badStimCodes, from = rawDataFiles[n]) %>% 
+      write_csv(paste0('./bad stim codes/bad_codes.csv'), append = TRUE)
+  }
+}
