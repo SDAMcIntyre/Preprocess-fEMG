9c344fccc6351bde1c456727a2036132a148c1e7
Sarah McIntyre <sarah.mcintyre@liu.se>
2019-08-02 15:52:48 +0200

    working script with latest functions, preprocesses a file and saves output for inspection

--- preprocess_femg.R
+++ preprocess_femg.R
@@ -6,6 +6,10 @@ source('preprocess_femg_functions.R')
 codedDataFolder <- './coded data/'
 codedDataFiles <- dir(codedDataFolder)
 
+IDformat <- '(s|p)[0-9]{3}'
+
+# ---- for saving pre-processed data ----
+outputFolder <- './preprocessed data/'
 
 # time in seconds
 prestim.sec <- 1.0 
@@ -15,79 +19,51 @@ win.sec <- 0.05
 
 flag.threshold <- 3 # multiple of SD
 prefixes <- c('Zyg', 'Cor')
+rawVariables <- c('Zyg.mV', 'Cor.mV')
 
 # ---- MAIN LOOP ---- 
 
-n <- 1
+n <- 3
 coded.data.file <- paste0(codedDataFolder,codedDataFiles[n])
 coded.femg.data <- read_csv(coded.data.file)
 
-win.samples <- win.sec/diff(coded.femg.data$Time.sec[1:2])
-
-# z-score data and flag extreme values
+# z-score data and flag extreme transitions
 pp.femg.data <- coded.femg.data %>% 
-  scale_and_flag(prefixes, win.samples, flag.threshold) %>% 
+  scale_and_flag(prefixes, win.sec, flag.threshold) %>% 
   filter(trialNo > 0 & 
            stimTime.sec >= -prestim.sec &
            stimTime.sec < stimulus.sec) 
 
-# how many flagged trials
-pp.femg.data %>% 
-  filter(stimTime.sec >= -baseline.sec) %>% 
-  group_by(trialNo) %>% 
-  summarise(flagged = any(flag.Zyg) ) %>% 
-  group_by(flagged) %>% 
-  tally() %>% 
-  mutate(pc = 100*n/sum(n)) -> Zyg.n.flagged
+# summary of trials with flags and find alternate baselines
+trials <- summarise_flagged_trials(pp.femg.data, prefixes, baseline.sec)
+glimpse(trials)
 
-# look at flagged zyg trials
-if (Zyg.n.flagged %>% filter(flagged) %>% pull(n) > 0) {
-  
-  pp.femg.data %>% 
-    filter(stimTime.sec >= -baseline.sec & flag.Zyg) %>% 
-    distinct(trialNo) %>% 
-    pull(trialNo) -> flagged.trials
-  
-  windows(20,20)
-  pp.femg.data %>% 
-    filter(stimTime.sec >= -baseline.sec & trialNo %in% flagged.trials) %>%
-    plot_flagged_femg_trials(Zyg.mV, flag.Zyg, win.sec) +
-    scale_x_continuous(breaks = function(x) seq(-baseline.sec, stimulus.sec, by = win.sec*4),
-                       minor_breaks = function(x) seq(-baseline.sec, stimulus.sec, by = win.sec))
-  ggsave(paste0('./FOR INSPECTION/',codedDataFiles[n],'_flagged_trials.png'))
-  dev.off()
-  tibble(code = flagged.trials, from = codedDataFiles[n]) %>% 
-    write_csv(paste0('./FOR INSPECTION/flagged_trials.csv'), append = TRUE)
+# save the flagged trials for inspection
+ID <- str_extract(codedDataFiles[n], IDformat)
+for (varPF in prefixes) {
+  if (trials[[varPF]]$nFlaggedTrials > 0) {
+    
+    for (thisTrial in trials[[varPF]]$allFlaggedTrials) {
+      thisBaseline <- trials[[varPF]]$alt.baselines %>% 
+        filter(trialNo == thisTrial) %>% select(2:3) %>% unlist() %>% na.omit()
+      if (length(thisBaseline) > 0) { note <- 'flagged_bl'} else {
+        note <- 'flagged'
+      }
+      
+      windows(15,2)
+      pp.femg.data %>% 
+        filter(stimTime.sec >= -prestim.sec & 
+                 trialNo %in% thisTrial) %>%
+        plot_flagged_femg_trials(y = str_subset(rawVariables,varPF), 
+                                 flag = paste(varPF, 'flagged', sep = '.'), win.sec,
+                                 baseline = thisBaseline) +
+        labs(title = paste(ID, varPF), x = 'Time (seconds)', y = str_subset(rawVariables,varPF))
+      ggsave(paste0('./FOR INSPECTION/',ID,'_',thisTrial,'_',varPF,'_',note,'.png')) 
+      dev.off()
+      tibble(code = thisTrial, variable = varPF, note = note, from = codedDataFiles[n]) %>% 
+        write_csv(paste0('./FOR INSPECTION/flagged_trials.csv'), append = TRUE)
+    }
+  }
 }
 
-# try to find better baseline periods
-if (Zyg.n.flagged %>% filter(flagged) %>% pull(pc) > 20) print(pc)
 
-pp.femg.data %>% 
-  filter(stimTime.sec >= 0 & flag.Zyg) %>% 
-  distinct(trialNo) %>% 
-  pull(trialNo) %>% 
-  setdiff(x = flagged.trials) -> baseline.only
-
-if (length(baseline.only > 0)) {
-  pp.femg.data %>% 
-    filter(trialNo == baseline.only[1] & stimTime.sec < 0) %>% 
-    mutate( new.bl = roll_maxr(flag.Zyg, 200, fill = NA) == 0) %>% 
-    filter(new.bl) %>% pull(stimTime.sec) %>% max() -> new.bl.end
-  
-  pp.femg.data %>% 
-    filter(stimTime.sec >= -prestim.sec & trialNo %in% baseline.only) %>%
-    plot_flagged_femg_trials(Zyg.mV, flag.Zyg, win.sec) +
-    scale_x_continuous(breaks = function(x) seq(-prestim.sec, stimulus.sec, by = win.sec*4),
-                       minor_breaks = function(x) seq(-prestim.sec, stimulus.sec, by = win.sec)) +
-    geom_vline(xintercept = c(new.bl.end - baseline.sec, new.bl.end), colour = '#41ab5d', size = 1) 
-}
-
-# look at flagged cor trials
-pp.femg.data %>% 
-  filter(stimTime.sec >= -baseline.sec) %>%
-  plot_flagged_femg_trials(Cor.z, flag.Cor)
-
-
-flagged.baseline.trials <- pp.femg.data %>% 
-  filter (flag.Zyg | flag.Cor)
