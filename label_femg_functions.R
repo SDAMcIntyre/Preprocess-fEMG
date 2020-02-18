634613bc9264957e5675bc6eb207c9e0776c9837
Sarah McIntyre <sarah.mcintyre@liu.se>
2020-02-18 17:00:04 +0100

    now handles instantaneous stimulus marker at onset

--- label_femg_functions.R
+++ label_femg_functions.R
@@ -3,7 +3,7 @@ library(readxl)
 library(RcppRoll)
 
 read_acq_text <- function(fileName, keepChannels) {
-
+  print(paste('Reading file', fileName))
   # extract numbers from 2nd row to get sample duration in ms and convert to sampling rate
   sampRate.Hz <- fileName %>% 
     read_lines(skip = 1, n_max = 1) %>% 
@@ -20,18 +20,8 @@ read_acq_text <- function(fileName, keepChannels) {
   # read channel names 
   # just get the odd ones for the names (even rows are measurement units)
   raw.ChannelNames <- read_lines(fileName,
-                                 skip = 3, n_max = 2*raw.nChannels)[seq(1,raw.nChannels*2, 2)]
-  
-  # all channels
-  # rawAcqData <- read_csv(fileName,
-  #                        skip = raw.nChannels*2+5, col_names = FALSE)[,1:raw.nChannels]
-  # names(rawAcqData) <- paste(raw.ChannelNames, seq_along(raw.ChannelNames), sep = '.')
-  # rawAcqData$time <- seq_along(rawAcqData[,1])
-  # rawAcq <- gather(rawAcqData, key = 'channel', value = 'value', -time)
-  # windows()
-  # ggplot(rawAcq) + 
-  #   geom_line(aes(x = time, y = value )) +
-  #   facet_grid(channel~., scale = 'free_y')
+                                 skip = 3, 
+                                 n_max = 2*raw.nChannels)[seq(1,raw.nChannels*2, 2)]
   
   # get the column numbers for the data and stimulus channels by matching the names
   keepColumns <- raw.ChannelNames %>% 
@@ -39,7 +29,9 @@ read_acq_text <- function(fileName, keepChannels) {
   
   # read in the data, keep only the wanted channels
   rawAcqData <- read_csv(fileName,
-                           skip = raw.nChannels*2+5, col_names = FALSE)[,keepColumns]
+                         skip = raw.nChannels*2+5, 
+                         col_names = FALSE,
+                         col_types = cols())[,keepColumns]
   # put the names back
   names(rawAcqData) <- raw.ChannelNames[keepColumns]
   
@@ -47,33 +39,45 @@ read_acq_text <- function(fileName, keepChannels) {
   rawAcqData <- rawAcqData %>% 
     mutate(Time.sec = seq(0,n()-1)/sampRate.Hz)
   
-  rawAcqData %>% 
-    gather(key = 'channel', value = 'value', -Time.sec) %>% 
-    ggplot() + 
-    geom_line(aes(x = Time.sec, y = value )) +
-    facet_grid(channel~., scale = 'free_y')
-  
   return(rawAcqData)
 }
 
+diff_from_prev <- function(x) { c(TRUE, diff(x) != 0) }
+diff_from_next <- function(x) { c(diff(x) != 0, TRUE) }
 
-denoise <- function(x, nsamples) {
-  flagged <- rep_along(x,FALSE)
+denoise <- function(x, nsamples = 'auto', ignoreValues = c(0)) {
+  if (nsamples == 'auto') {
+    # look at the data to find out how long the stimulus signal is
+    runs <- rle(x)
+    raw.stim.runs <- tibble(lengths = runs$lengths,
+                            stimcode = runs$values)
+    stimcode.nsamples <- raw.stim.runs %>% 
+      filter(stimcode %in% ignoreValues == FALSE) %>% 
+      pull(lengths) %>% median()
+    nsamples = ceiling(stimcode.nsamples/2)
+  }
+  
   # look forwards
+  #get the median of the last window in the dataset
   last.window.median <- median(x[(length(x)-nsamples):length(x)])
+  
+  # get the rolling median of the data, left-aligned, pad it with the 
+  # median of the last window so that it is the same length as the data
   next.n.med <- roll_medianl(x, nsamples, fill = last.window.median)
-  # shift it one back so it doesn't include itself
+  
+  # shift the medians back by one position so that each value in the 
+  # dataset aligns with the median of the window of data ahead of it ("looking ahead")
   next.n.med <- c(next.n.med[-1], next.n.med[length(next.n.med)])
   
-  # look backwards
+  # look backwards (do the same as above but right aligned medians)
   first.window.median <- median(x[1:nsamples])
-  last.n.med <- roll_medianr(x, nsamples, fill = first.window.median)
+  prev.n.med <- roll_medianr(x, nsamples, fill = first.window.median)
   #shift it one forwards so it doesn't include itself
-  last.n.med <- c(last.n.med[1], last.n.med[-length(last.n.med)])
+  prev.n.med <- c(prev.n.med[1], prev.n.med[-length(prev.n.med)])
   
   # check if it's both different from the median of the previous n samples and 
   #from the median of the next n samples
-  flagged <- x != last.n.med & x != next.n.med
+  flagged <- x != prev.n.med & x != next.n.med
 
   corrected <- x
   corrected[flagged] <- next.n.med[flagged]
@@ -82,56 +86,34 @@ denoise <- function(x, nsamples) {
   
 }
 
-clean_acq_stim_codes <- function(data, stimChannel, usedStimCodes, knownNoiseCodes, offCode = 0) {
-  diff_from_prev <- function(x) c(TRUE, diff(x) != 0)
-  diff_from_next <- function(x) c(diff(x) != 0, TRUE)
-  
-  stimCodedData <- data %>% 
+clean_acq_stim_codes <- function(femgData, stimChannel, usedStimCodes, knownNoiseCodes, offCode = 0, nsamples = 'auto') {
+
+  denoised <- denoise(femgData[[stimChannel]], nsamples = nsamples, 
+                      ignoreValues = c(offCode, knownNoiseCodes))
+  stimCodedData <- femgData %>% 
     mutate(
       # try to flag and correct unknown noise/debounce errors:
-      Stim.flag.noise = denoise(.data[[stimChannel]], 100)$flagged,
-      StimCode.corrected = denoise(.data[[stimChannel]], 100)$corrected,
+      Stim.flag.noise = denoised$flagged,
+      StimCode.corrected = denoised$corrected,
       #set known noise codes to 0
-      StimCode.corrected = replace(StimCode.corrected, StimCode.corrected %in% knownNoiseCodes, stim.OffCode),
-      # is the code different to the previous one? 
-      transition.start = diff_from_prev(StimCode.corrected),
-      transition.end = diff_from_next(StimCode.corrected),
+      StimCode.corrected = replace(StimCode.corrected, # values to replace
+                                   StimCode.corrected %in% knownNoiseCodes, # condition
+                                   stim.OffCode), # replace with
       # is the stim code an unexpected one?
       unexpected = StimCode.corrected %in% usedStimCodes == FALSE
     )
-  
-  # get trial numbers from indices of transition points
-  newTrials <- c(0, with(stimCodedData, 
-                         which(transition.end & 
-                                 StimCode.corrected != offCode)))
-  
-  # fill in all trial numbers 
-  stimCodedData <- stimCodedData %>% mutate(trialNo = 0)
-  for (n in seq_along(newTrials)[-length(newTrials)]) {
-    stimCodedData <- stimCodedData %>% 
-      mutate(trialNo = replace(trialNo, 
-                               (newTrials[n]+1):newTrials[n+1], 
-                               values = n))
-  }
-  
-  # add phase info (pre-stim/stim)
-  stimCodedData <- stimCodedData %>% 
-    mutate(phase = ifelse(StimCode.corrected != offCode, 
-                 'stimulus', 'prestim'),
-           phase = replace(phase, trialNo == 0, NA))
-  
-  # time relative to stimulus onset
-  stimCodedData <- stimCodedData %>%
-    group_by(trialNo) %>%
-    mutate(stimTime.sec = Time.sec - min(Time.sec[phase!='prestim']))
-  
   return(stimCodedData)
-}
+}  
 
-plot_stim_code_sequence <- function(data, stimVar) {
-  unexpectedCodes <- unique(filter(data,unexpected)$StimCode.corrected)
-  data %>% 
-    ggplot(aes(Time.sec, .data[[stimVar]])) +
+plot_stim_code_sequence <- function(femgData, stimChannel) {
+  unexpectedCodes <- femgData %>% filter(unexpected) %>% 
+    .[[stimChannel]] %>% unique()
+  femgData %>%
+    mutate(transition.start = diff_from_prev(.[[stimChannel]]),
+           transition.end = diff_from_next(.[[stimChannel]])
+           ) %>%
+    filter(transition.start | transition.end) %>% 
+    ggplot(aes(Time.sec, .data[[stimChannel]])) +
     geom_path() +
     geom_point(aes(colour = unexpected)) +
     scale_colour_manual(values = c('grey','red')) +
@@ -140,47 +122,171 @@ plot_stim_code_sequence <- function(data, stimVar) {
          x = 'Time (sec)') 
 }
 
-compare_stim_face_emoji_expt <- function(femgData, stimFile) {
+
+compare_stim_face_emoji_expt <- function(femgData, stimChannel, offCode = 0, stimFile, fillStimCodes = FALSE) {
   
   # read in stim file
-  stimData <- read_tsv(stimFile, skip = 3) %>%
-    mutate(StimCode = as.numeric(`Pic(num)`),
-           Time.sec = Time/10000) %>% 
-    filter(`Event Type` == 'Picture' & is.na(StimCode) == FALSE) %>% 
-    select(StimCode, Time.sec)
+  stimData <- suppressWarnings(
+    read_tsv(stimFile, skip = 3, col_types = '__c_n__d__d_____')
+               ) %>%
+    rename('StimCode' = `Pic(num)`) %>% 
+    mutate(Start.sec = Time/10000,
+           Duration.sec = Duration/10000,
+           End.sec = Start.sec + Duration.sec ) %>% 
+    filter(`Event Type` == 'Picture' & !is.na(StimCode) & StimCode != offCode) 
+
+  # get time of first stimulus in stimulus file
+  stim.StartTime <- stimData %>% filter(StimCode != offCode) %>% 
+    summarise(start = min(Start.sec)) %>% pull(start)
   
-  # time offset
-  stim.Start <- stimData %>% 
-    filter(StimCode > 0) %>% 
-    summarise(start = min(Time.sec))
+  # get time of first stimulus in femg file
+  femgData <- femgData %>% ungroup() %>% 
+    mutate(
+      transition.start = diff_from_prev(.[[stimChannel]]),
+      transition.end = diff_from_next(.[[stimChannel]]),
+    )
+  femg.StartTime <- femgData %>% 
+    filter(transition.start & .[[stimChannel]] != offCode) %>% 
+    summarise(start = min(Time.sec)) %>% pull(start)
   
-  femg.Start <- femgData %>% 
-    ungroup() %>% 
-    filter(transition.start & StimCode.corrected > 0) %>% 
-    summarise(start = min(Time.sec))
+  # time offset between femg and stim files
+  offset <- femg.StartTime - stim.StartTime
   
-  offset <- femg.Start$start - stim.Start$start
+  # align stimulus sequence to first stim in femg file
+  stimAligned <- stimData %>% 
+    select(StimCode, Start.sec, Duration.sec, End.sec) %>% 
+    mutate(Start.sec = Start.sec + offset,
+           End.sec = End.sec + offset)
   
   # stim sequence in femg file
-  femg.Seq <- filter(femgData,transition.start)$StimCode.corrected
+  femg.Seq <- femgData %>% 
+    filter(transition.start & .[[stimChannel]] != offCode) %>% 
+    .[[stimChannel]]
   
   # is the sequence in the femg data file the same as in the stim data file?
-  synced <- sum(abs(stimData$StimCode - femg.Seq[-length(femg.Seq)])) == 0
+  synced <- sum(abs(stimData$StimCode - femg.Seq)) == 0
+  
+  # does the timing of the start of the stimulus match?
+  femg.Start <- femgData %>% 
+    filter(transition.start & .[[stimChannel]] != offCode) %>% 
+    pull(Time.sec)
+  
+  startOffsets <- stimAligned$Start.sec - femg.Start
+  
+  # does the timing of the end of the stimulus match?
+
+  femg.End <- femgData %>% 
+    filter(transition.end & .[[stimChannel]] != offCode) %>% 
+    pull(Time.sec) 
+  
+  endOffsets <- stimAligned$End.sec - femg.End 
   
   comparisonPlot <- ggplot() +
     geom_path(data = filter(femgData, transition.start | transition.end),
-              aes(x = Time.sec, y = StimCode.corrected)) +
-    geom_text(data = filter(femgData, transition.start & StimCode.corrected > 0),
-              aes(x = Time.sec, y = StimCode.corrected + 40, label = StimCode.corrected),
+              aes(x = Time.sec, y = .data[[stimChannel]])) +
+    geom_text(data = filter(femgData, transition.start & .data[[stimChannel]] != offCode),
+              aes(x = Time.sec, y = .data[[stimChannel]] + 40, label = .data[[stimChannel]]),
               colour = 'black') +
-    geom_point(data = stimData,
-               aes(x = Time.sec + offset, y = StimCode),
+    geom_point(data = stimAligned,
+               aes(x = Start.sec, y = StimCode),
                colour = 'blue', shape = 3) +
-    geom_text(data = filter(stimData, StimCode > 0),
-              aes(x = Time.sec + offset, y = StimCode + 20, label = StimCode),
-              colour = 'blue')
+    geom_text(data = stimAligned,
+              aes(x = Start.sec, y = StimCode + 20, label = StimCode),
+              colour = 'blue') +
+    geom_point(data = stimAligned,
+               aes(x = End.sec, y = offCode),
+               colour = 'blue', shape = 4) +
+    labs(title = 'Black: stim codes in femg file; Blue: stim codes in stim file', y = stimChannel)
   
-  return(list('stimSeqData' = stimData, 'offset' = offset, 'synced' = synced, 'comparisonPlot' = comparisonPlot))
+  output <- list('stimSeqData' = stimAligned, 
+                 'synced' = synced, 
+                 'startOffsets' = startOffsets, 
+                 'endOffsets' = endOffsets, 
+                 'comparisonPlot' = comparisonPlot)
+
+    if (fillStimCodes) {
+    stimFilledData <- femgData %>% mutate(StimCode.filled = .[[stimChannel]])
+    for (n in seq_along(stimAligned$StimCode)) {
+      tofill <- which(
+        femgData$Time.sec > femg.Start[n] & 
+        femgData$Time.sec <= femg.Start[n] + stimAligned$Duration.sec[n]
+        )
+      stimFilledData[tofill,'StimCode.filled'] <- stimAligned$StimCode[n]
+    }
+    
+    stimFilledData <- stimFilledData %>% 
+      mutate(
+        transition.start = diff_from_prev(StimCode.filled),
+        transition.end = diff_from_next(StimCode.filled),
+      )
+    
+    filledPlot <- ggplot() +
+      geom_path(data = filter(stimFilledData, transition.start | transition.end),
+                aes(x = Time.sec, y = StimCode.filled), colour = 'darkgreen') +
+      geom_text(data = filter(stimFilledData, transition.start & StimCode.filled != offCode),
+                aes(x = Time.sec, y = StimCode.filled + 40, label = StimCode.filled),
+                colour = 'darkgreen') +
+      geom_point(data = stimAligned,
+                 aes(x = Start.sec, y = StimCode),
+                 colour = 'blue', shape = 3) +
+      geom_text(data = stimAligned,
+                aes(x = Start.sec, y = StimCode + 20, label = StimCode),
+                colour = 'blue') +
+      geom_point(data = stimAligned,
+                 aes(x = End.sec, y = offCode),
+                 colour = 'blue', shape = 4) +
+      labs(title = 'Green: stim codes in femg file (duration filled from stim file); Blue: stim codes in stim file', y = stimChannel) 
+    
+    output$stimFilledData <- stimFilledData
+    output$filledPlot <- filledPlot
+    
+    } else {
+      startmed <- median(startOffsets)
+      endmed <- median(endOffsets)
+      if (endmed > 3*startmed) {
+        warning(paste('compare_stim_face_emoji_expt()\n',
+                      'offsets for the end of the stimulus are between', 
+                      prettyNum(min(endOffsets)), 'and', prettyNum(max(endOffsets)), 
+                      'seconds (median =', prettyNum(endmed), 
+                      'seconds). \nDo you want \'fillStimCodes = TRUE\'?'))
+      }
+    }
+  
+  return(output)
   
 }
 
+label_femg_data <- function(femgData, stimChannel, offCode) {
+  
+  # is the code different to the previous one? 
+  stimLabelledData <- femgData %>% 
+    mutate(
+      transition.start = diff_from_prev(femgData[[stimChannel]]),
+      transition.end = diff_from_next(femgData[[stimChannel]])
+    )
+  # get trial numbers from indices of transition points
+  newTrials <- c(0, which(stimLabelledData$transition.end &
+                            stimLabelledData[[stimChannel]] != offCode))
+  
+  # fill in all trial numbers 
+  stimLabelledData <- stimLabelledData %>% mutate(trialNo = 0)
+  for (n in seq_along(newTrials)[-length(newTrials)]) {
+    stimLabelledData <- stimLabelledData %>% 
+      mutate(trialNo = replace(trialNo, 
+                               (newTrials[n]+1):newTrials[n+1], 
+                               values = n))
+  }
+  
+  # add phase info (pre-stim/stim)
+  stimLabelledData <- stimLabelledData %>% 
+    mutate(phase = ifelse(stimLabelledData[[stimChannel]] != offCode, 
+                          'stimulus', 'prestim'),
+           phase = replace(phase, trialNo == 0, NA))
+  
+  # time relative to stimulus onset
+  stimLabelledData <- stimLabelledData %>%
+    group_by(trialNo) %>%
+    mutate(stimTime.sec = Time.sec - min(Time.sec[phase!='prestim']))
+  
+  return(stimLabelledData %>% ungroup)
+}
